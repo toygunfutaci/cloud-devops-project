464c46d0359a0728e0070de45ecc934c4bec8dc0
toygunfutaci <toygun.futaci@gmail.com>
2025-08-22 13:02:08 +0100

    Add Jenkins CI/CD pipeline

---
+++ tests/IntegrationTests/WeatherIntegrationTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+public class WeatherEndpointTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public WeatherEndpointTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            // Avoid HTTPS redirect causing 30x in tests if https redirection is enabled
+            AllowAutoRedirect = true
+        });
+    }
+
+    [Fact]
+    public async Task GET_weatherforecast_returns_ok()
+    {
+        var resp = await _client.GetAsync("/weatherforecast");
+        Assert.Equal(HttpStatusCode.OK, resp.StatusCode);
+    }
+}
---
+++ tests/UnitTests/ExampleUnitTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace UnitTests;
+
+public class ExampleUnitTests
+{
+    [Fact]
+    public void Math_addition_works()
+    {
+        Assert.Equal(4, 2 + 2);
+    }
+}
